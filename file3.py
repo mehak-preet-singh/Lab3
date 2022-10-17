68ff920e3d3f9dd77128e7d3632330c63b02219d
mehak-preet-singh <115911437+mehak-preet-singh@users.noreply.github.com>
2022-10-16 22:21:22 -0400

    Update file3.py

--- file3.py
+++ file3.py
@@ -2,6 +2,7 @@ A function that will write a text file to your PC with your name:
   
 #!/usr/bin/env python3
 
+#defining a function in which we further open the file and write name in it, and then closing the file.
 def put_a_name_to_file():
     f = open("name.txt" , "w+")
     f.write("Mehak Preet Singh")
